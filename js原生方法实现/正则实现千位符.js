0a56737f911ec2f05987f4078b7493a2c67a665c
yangming <wawa754870370@qq.com>
2017-11-09 12:18:34 +0800

    no message

--- js原生方法实现/正则实现千位符.js
+++ js原生方法实现/正则实现千位符.js
@@ -5,3 +5,13 @@ var thousandBitSeparator = function(numStr){
      });
  }
 thousandBitSeparator('2132131231231');
+
+
+
+
+function comma(num) {
+    var source = String(num).split(".");//按小数点分成2部分
+        source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)','ig'),"$1,");//只将整数部分进行都好分割
+    return source.join(".");//再将小数部分合并进来
+}
+comma(-123456.789);//-123,456.789
