c3c125e26941913a8f5f175d7eb0fa307dd53899
杨明 <yangming.mingdi@bytedance.com>
2019-11-27 18:51:40 +0800

    update: 1. 增加 点击展示与隐藏 2. 针对定位获取实际文档高度

--- node-drag-VUE拖拽规则树实现/src/components/HelloWorld.vue
+++ node-drag-VUE拖拽规则树实现/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <div v-for="(item, index) in data" :key="item.key">
+    <div :class="`parent parent-${item.key}`" v-for="(item, index) in data" :key="item.key">
       <div
         :class="`drag drag-${item.key} ${showClass(item)}`"
         draggable="true"
@@ -9,12 +9,21 @@
         @dragenter="dragenter"
         @dragend="dragend"
         @dragover="dragover"
+        @click="showChildrenSwitch([...indexList, index])"
         :data-key="item.key"
         :data-index-list="[...indexList, index]"
       >
         {{item.name}}
       </div>
-      <Node class="node" v-show="item.children" :indexList="[...indexList, index]" :data="item.children" />
+      <!-- <div class="model-hover-box">
+        <div class="model-hover-tag">编辑</div>
+        <div class="model-hover">
+          <p>添加子节点</p>
+          <p>删除节点</p>
+        </div>
+      </div> -->
+      
+      <Node v-if="item.showChildren"  class="node" :indexList="[...indexList, index]" :data="item.children" />
     </div>
   </div>
 </template>
@@ -39,8 +48,12 @@ export default {
     Node
   },
   methods: {
+    showChildrenSwitch(indexList) {
+      this.$store.commit('changeShowChildren', indexList);
+    },
     showClass(item) {
       if (this.$store.state.targetNode.key === item.key && !this.$store.state.isOutside) {
+       
         if(this.$store.state.isTop) {
           return 'showBorderTop';
         } else {
@@ -64,7 +77,7 @@ export default {
         key: event.target.dataset.key,
         className: event.target.className,
       };
-      // 由于 enter事件都是发生在leave前 所以用count来记录 是否真正的离开了
+       // 由于 enter事件都是发生在leave前 所以用count来记录 是否真正的离开了
       const count = this.$store.state.count + 1;
       this.$store.commit('changeState', {key: 'targetNode',value: targetNode});
       this.$store.commit('changeState', {key: 'count',value: count});
@@ -83,22 +96,36 @@ export default {
     },
     // enter元素后 监听鼠标的移动 判断 拖拽元素 是移动到 当前元素 上方还是 下方
     dragover(event) {
-      const element = event.toElement;
-      const isTop = !!(event.pageY <= (element.offsetTop + element.clientHeight/2));
+      let element = event.toElement;
+      let x = 0, y = 0;
+      const height = element.offsetHeight/2;
+      while (element != null) {
+          y += element.offsetTop;
+          element= element.offsetParent;
+      }
+      const isTop = !!(event.pageY <= (y + height));
       this.$store.commit('changeState', {key: 'isTop',value: isTop });
     },
     // end 结束时 拿到鼠标坐标点 判断是否在 拖拽区域内 在则进行 移动操作 不在则清除 节点信息
     dragend(event){
       const parent = document.querySelector('.drag-box');
-      const isInX = !!(parent.offsetLeft <= event.pageX && event.pageX <= (parent.offsetLeft + parent.clientWidth));
-      const isInY = !!(parent.offsetTop <= event.pageY && event.pageY <= (parent.offsetTop + parent.clientHeight));
+      const isInX = !!(parent.offsetLeft <= event.pageX && event.pageX <= (parent.offsetLeft + parent.offsetWidth));
+      const isInY = !!(parent.offsetTop <= event.pageY && event.pageY <= (parent.offsetTop + parent.offsetHeight));
       if(isInX && isInY) {
         const targetNode = this.$store.state.targetNode;
         const className = targetNode.className.split(' ')[1];
-        const element = document.querySelector(`.${className}`);
-        const targetList = element.dataset.indexList.split(',');
+        // 或区域元素的父节点
+        const parentElement = document.querySelector(`.parent-${targetNode.key}`);
+        const node = this.$store.state.node;
+        const nodeElemnt = document.querySelector(`.parent-${node.key}`);
+        const targetElement = document.querySelector(`.${className}`);
+        // 判断移动到的节点 是否 在 拖拽节点内部，如果在 则拒绝此次 拖拽
+        if(nodeElemnt.contains(parentElement)) {
+          return
+        }
+        const targetList = targetElement.dataset.indexList.split(',');
         const souceList = event.target.dataset.indexList.split(',');
-        this.$store.commit('cahngeData', {targetList, souceList});
+        this.$store.commit('changeData', {targetList, souceList});
         this.$store.commit('changeState', {key: 'targetNode',value: {}});
       } else {
         this.$store.commit('changeState', {key: 'targetNode',value: {}});
@@ -121,6 +148,10 @@ export default {
 .drag {
   padding: 10px;
   border: 2px solid #fff;
+  cursor: pointer;
+  position: relative;
+  left: 10px;
+  top: 10px;
 }
 .drag:hover {
   background-color: rgba(0,0,0,.1);
@@ -132,4 +163,34 @@ export default {
 .showBorderBottom {
   border-bottom: 2px solid blue;
 }
+.parent {
+  position: relative;
+  /* display: flex;
+  flex-direction: column; */
+}
+/* .model-hover-box {
+  position: absolute;
+  z-index: 10;
+  right: 20px;
+  top: 0px;
+  cursor: pointer;
+}
+.model-hover-tag {
+  position: relative;
+  z-index: 9;
+}
+.model-hover-box:hover .model-hover {
+  display: block;
+}
+.model-hover {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  width: 100px;
+  padding: 10px;
+  display: none;
+  background-color: #fff;
+  border-radius: 2px;
+  text-align: center;
+} */
 </style>
--- node-drag-VUE拖拽规则树实现/src/store/index.js
+++ node-drag-VUE拖拽规则树实现/src/store/index.js
@@ -9,14 +9,17 @@ const store = new Vuex.Store({
             {
               key: '1',
               name: '节点1',
+              showChildren: false,
               children: [
                 {
                   key: '1-1',
                   name: '节点1-1',
+                  showChildren: false,
                   children: [
                     {
                       key: '1-1-1',
                       name: '节点1-1-1',
+                      showChildren: false,
                       children: [],
                     }
                   ]
@@ -24,20 +27,24 @@ const store = new Vuex.Store({
                 {
                   key: '1-2',
                   name: '节点1-2',
+                  showChildren: false,
                   children: [
                     {
                       key: '1-2-1',
                       name: '节点1-2-1',
+                      showChildren: false,
                       children: [],
                     },
                     {
                       key: '1-2-2',
                       name: '节点1-2-2',
+                      showChildren: false,
                       children: [],
                     },
                     {
                       key: '1-2-3',
                       name: '节点1-2-3',
+                      showChildren: false,
                       children: [],
                     }
                   ]
@@ -47,14 +54,17 @@ const store = new Vuex.Store({
             {
               key: '2',
               name: '节点2',
+              showChildren: false,
               children: [
                 {
                   key: '2-1',
                   name: '节点2-1',
+                  showChildren: false,
                   children: [
                     {
                       key: '2-1-1',
                       name: '节点2-1-1',
+                      showChildren: false,
                       children: [],
                     }
                   ]
@@ -62,20 +72,24 @@ const store = new Vuex.Store({
                 {
                   key: '2-2',
                   name: '节点2-2',
+                  showChildren: false,
                   children: [
                     {
                       key: '2-2-1',
                       name: '节点2-2-1',
+                      showChildren: false,
                       children: [],
                     },
                     {
                       key: '2-2-2',
                       name: '节点2-2-2',
+                      showChildren: false,
                       children: [],
                     },
                     {
                       key: '2-2-3',
                       name: '节点2-2-3',
+                      showChildren: false,
                       children: [],
                     }
                   ]
@@ -86,37 +100,50 @@ const store = new Vuex.Store({
         node: {},
         targetNode: {},
         isTop: false,
+        isOutside: true,
     },
     mutations: {
         changeState(state, data) {
-            state[data.key] = data.value;
+          state[data.key] = data.value;
         },
-        cahngeData(state, info) {
-            const { targetList,  souceList} = info;
-            const { data, isTop } = state;
-            let souceInfo = data;
-            const length = souceList.length;
-            for(let i = 0; i < length; i++) {
-                if(length - 1 === i) {
-                    souceInfo = souceInfo.splice(souceList[i], 1);
-                } else {
-                    souceInfo = souceInfo[souceList[i]].children;
-                }
+        changeData(state, info) {
+          const { targetList,  souceList } = info;
+          const { data, isTop } = state;
+          let souceInfo = data;
+          const length = souceList.length;
+          for(let i = 0; i < length; i++) {
+              if(length - 1 === i) {
+                  souceInfo = souceInfo.splice(souceList[i], 1);
+              } else {
+                  souceInfo = souceInfo[souceList[i]].children;
+              }
+          }
+          let targetInfo = data;
+          targetList.forEach((v, index) => {
+              if(index === targetList.length -1) {
+                  if(isTop) {
+                      targetInfo.splice(v, 0, souceInfo[0]);
+                  } else {
+                      targetInfo.splice(v+1, 0, souceInfo[0]);
+                  }
+                  
+              } else {
+                  targetInfo = targetInfo[targetList[index]].children;
+              }
+          })
+          state.data = data;
+        },
+        changeShowChildren(state, indexList) {
+          const { data } = state;
+          let res = data;
+          for(let i = 0; i < indexList.length; i++) {
+            if(i === indexList.length - 1) {
+              res[indexList[i]].showChildren = !res[indexList[i]].showChildren;
+            } else {
+              res = res[indexList[i]].children;
             }
-            let targetInfo = data;
-            targetList.forEach((v, index) => {
-                if(index === targetList.length -1) {
-                    if(isTop) {
-                        targetInfo.splice(v, 0, souceInfo[0]);
-                    } else {
-                        targetInfo.splice(v+1, 0, souceInfo[0]);
-                    }
-                    
-                } else {
-                    targetInfo = targetInfo[targetList[index]].children;
-                }
-            })
-            state[data] = data;
+          }
+          // state.data = res;
         }
     }
 })
